2e95131b217b5b9b4026c4e276b566795f7d4ed0
Sumit kumar <sumit_k@mt.iitr.ac.in>
2025-08-12 23:08:18 +0530

    fraud_detection.py

--- fraud_detection.py
+++ fraud_detection.py
@@ -10,7 +10,7 @@ st.markdown("Please enter the transaction detailes and use the predict button")
 
 st.divider()
 
-transaction_type = st.selectbox("Transaction Type", ["PAYMENT", "TRANSFER", "CASE_OUT", "DEPOSIT"])
+transaction_type = st.selectbox("Transaction Type", ["PAYMENT", "TRANSFER", "CASH_OUT", "DEPOSIT"])
 amount = st.number_input("Amount", min_value = 0.0, value= 1000.0)
 oldbalanceOrg = st.number_input("Old Balance (Sender)", min_value = 0.0, value = 10000.0)
 newbalanceOrig = st.number_input("New Balance (Sender)", min_value=0.0, value=9000.0)
@@ -34,4 +34,5 @@ if st.button("Predict"):
     if prediction == 1:
         st.error("This transaction can be fraud")
     else:
+
         st.success("This transaction looks like it is not a fraud")
